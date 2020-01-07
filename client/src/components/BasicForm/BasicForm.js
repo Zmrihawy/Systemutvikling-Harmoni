90cef1c5f4a3e3cf775b9244d1f6591fbf1cd97c
SveinJH <43957538+SveinJH@users.noreply.github.com>
2020-01-07 14:21:21 +0100

    Registering event work

--- client/src/components/BasicForm/BasicForm.js
+++ client/src/components/BasicForm/BasicForm.js
@@ -5,7 +5,12 @@ const basicForm = props => {
         <>
             <h3>{props.title}</h3>
             <form onSubmit={props.clicked}>
-                <input type={props.inputType} />
+                <input
+                    type={props.inputType}
+                    value={props.value}
+                    name={props.name}
+                    onChange={props.changed}
+                />
                 <input type="submit" value="Neste" />
             </form>
         </>
