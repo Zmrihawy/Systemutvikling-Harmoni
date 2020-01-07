32a090feb6a3d6d19e1012fbbf12f9ceb0dc2e14
Hans Kristian Granli <hkgranli@stud.ntnu.no>
2020-01-07 15:20:06 +0100

    Added SEVERAL sql queries

--- server/src/dao/serverDao.js
+++ server/src/dao/serverDao.js
@@ -67,13 +67,71 @@ module.exports = class ServerDao extends Dao {
     }
 
     /**DELETE*/
-    deleteUser(){
+    deleteUser(sql , callback){
+        super.query(`DELETE FROM ${USER_TABLE} WHERE ${USER_EMAIL} = ?`, [sql], callback);
+    }
 
+    deleteRider(sql, callback){
+        super.query(`DELETE FROM ${RIDER_TABLE} WHERE ${RIDER_PERFORMANCE_ID} = ? AND ${RIDER_NAME}`, [sql], callback);
+    }
+
+    deleteEvent(sql, callback){
+        super.query(`DELETE FROM ${EVENT_TABLE} WHERE ${EVENT_ID}`, [sql], callback);
+    }
+
+    deleteTicket(sql, callback){
+        super.query(`DELETE FROM ${TICKET_TABLE} WHERE ${TICKET_NAME} = ? AND ${TICKET_EVENT_ID} = ?`, [sql.name, sql.eventId], callback);
+    }
+
+    deletePerformance(sql, callback){
+        super.query(`DELETE FROM ${PERFORMANCE_TABLE} WHERE ${PERFORMANCE_ARTIST_ID} = ?`, [sql], callback);
     }
 
     /**UPDATE*/
-    updateUsername() {
+    updateUsername(sql, callback) {
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_USERNAME} = ? WHERE ${USER_ID} = ?`, [sql.username, sql.userId], callback);
+    }
 
+    updatePassword(sql, callback){
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_PASSWORD} = ? WHERE ${USER_ID} = ?`, [sql.password, sql.userId], callback);
+    }
+
+    updateName(sql, callback){
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_FIRST_NAME} = ?, ${USER_LAST_NAME} = ? WHERE ${USER_ID} = ?`, [sql.firstName, sql.lastName, sql.userId], callback);
+    }
+
+    updateEmail(sql, callback){
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_EMAIL} = ? WHERE ${USER_ID}  = ?`, [sql.email, sql.userId], callback);
+    }
+
+    updatePhone(sql, callback){
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_PHONE} = ? WHERE ${USER_ID} = ?`, [sql.phone, sql.userId], callback);
+    }
+
+    updateUser(sql, callback){
+        let userAtributes = [sql.username, sql.salt,sql.email,sql.phone,sql.firstName,sql.lastName, sql.userId];
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_USERNAME} = ?, ${USER_EMAIL} = ?, ${USER_PHONE} = ?, ${USER_FIRST_NAME}, ${USER_LAST_NAME} = ? WHERE ${USER_ID} = ?`,
+        userAtributes, callback);
+    }
+
+    updateTicket(sql, callback){
+        super.query(`UPDATE ${TICKET_TABLE} SET ${TICKET_PRICE} = ?, ${TICKET_AMOUNT} = ?, ${TICKET_DESCRIPTION} = ?  WHERE ${TICKET_NAME} = ? AND ${TICKET_EVENT_ID} = ? `
+        [sql.ticketPrice, sql.ticketDescription, sql.ticketName, sql.ticketEventId], callback);
+    }
+
+    updateRider(sql, callback){
+        super.query(`UPDATE ${RIDER_TABLE} SET ${RIDER_NAME} = ?, ${RIDER_AMOUNT} = ? WHERE ${RIDER_PERFORMANCE_ID} and ${RIDER_NAME} = ?`, 
+        [sql.name, sql.ammount, sql.performanceId, sql.oldName], callback);
+    }
+
+    updatePerformance(sql, callback){
+        super.query(`UPDATE ${PERFORMANCE_TABLE} SET ${PERFORMANCE_START_TIME} = ?, ${PERFORMANCE_END_TIME} = ?, ${PERFORMANCE_CONTRACT} = ? WHERE ${PERFORMANCE_ID} = ?`,
+        [sql.startTime, sql.endTime, sql.contract, sql.performanceId], callback);
+    }
+
+    updateEvent(sql, callback){
+        super.query(`UPDATE ${EVENT_TABLE} SET ${EVENT_NAME} = ?, ${EVENT_HOST_ID} = ?, ${EVENT_ACTIVE} = ?, ${EVENT_LOCATION} = ?, ${EVENT_START_TIME} = ?, ${EVENT_END_TIME} = ? WHERE ${EVENT_ID} = ?`,
+        [sql.eventHostId, sql.eventActive, sql.eventLocation, sql.eventStartTime, sql.endTime, sql.eventId], callback);
     }
 
 };
