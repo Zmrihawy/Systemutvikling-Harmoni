8e28f37f4004bd7fcaecbdf1b81bedf4a9714c4b
Torje Dahll-Larssøn Thorkildsen <tdthorki@stud.ntnu.no>
2020-01-07 11:03:26 +0100

    Added some more dao functions

--- server/src/dao/Serverdao.js
+++ server/src/dao/Serverdao.js
@@ -1,55 +1,54 @@
 import Dao from './dao.js';
 
 const arr_id = "arr_id";
-const bruker_id = "bruker_id"
+const bruker_id = "bruker_id";
+const email = "email";
 
 module.exports = class ServerDao extends Dao {
 
-    getContract(sql, callback){
+    getContract(sql, callback) {
         super.query(`SELECT * FROM Kontrakt WHERE ${arr_id} = ? AND ${bruker_id} = ?`, [sql.arr, sql.artist], callback);
     }
 
-    getArrContracts(sql, callback){
+    getArrContracts(sql, callback) {
         super.query(`SELECT * FROM Kontrakt WHERE ${arr_id} = ?`, [sql], callback);
     }
 
-    getUser(sql, callback){
-        super.query(`SELECT * FROM Bruker WHERE email = ?`, [sql], callback);
+    getUser(sql, callback) {
+        super.query(`SELECT * FROM Bruker WHERE ${email} = ?`, [sql], callback);
     }
 
-    getUsers(sql, callback){
+    getUsers(sql, callback) {
         super.query(`SELECT * FROM Bruker`, [], callback);
     }
 
-    getTickets(sql, callback){
+    getTickets(sql, callback) {
         super.query(`SELECT * FROM Bilett WHERE ${arr_id} = ?`, [sql], callback);
     }
 
-    getRiders(sql, callback){
+    getRiders(sql, callback) {
         super.query(`SELECT * FROM Rider WHERE ${arr_id} = ? AND ${bruker_id} = ?`, [sql.arr, sql.artist], callback);
     }
 
-    getEvent(sql, callback){
+    getEvent(sql, callback) {
         super.query(`SELECT * FROM Arrangement WHERE ${arr_id} = ?`, [sql], callback);
     }
 
-    getActiveEvents(sql, callback){
-        super.query(`SELECT * FROM Arrangement WHERE ${arr_id} = ? AND active = true`, [sql], callback);
+    getActiveEvents(sql, callback) {
+        super.query(`SELECT * FROM Arrangement WHERE ${arr_id} = ? AND aktiv = true`, [sql], callback);
     }
 
-    getAllEvents(callback){
+    getAllEvents(callback) {
         super.query(`SELECT * FROM Arrangement`, [], callback);
     }
 
-    getUsersEvents(sql, callback){
-        super.query(`SELECT * FROM Arrangement LEFT JOIN `)
+    getUsersEvents(sql, active, callback) {
+        super.query(`SELECT * FROM Arrangement a JOIN Bruker b ON a.${arr_id} = b.${bruker_id} WHERE aktiv = ?`, [sql.bruker, active], callback);
     }
 
-    getEventWithArtist(sql, callback){
-
+    getEventWithArtist(sql, callback) {
+        this.getUsersEvents(sql, true, callback);
     }
-
-
 
 
 }
