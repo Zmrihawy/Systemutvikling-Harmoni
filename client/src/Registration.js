50979f81d0ad017daccf1b24d3eadf6e76ca7587
Zmrihawy <zmrihawy@stud.ntnu.no>
2020-01-08 15:14:03 +0100

    #Update registration page

--- client/src/Registration.js
+++ client/src/Registration.js
@@ -10,9 +10,14 @@ export default class Registration extends Component {
             <div className="login-page">
             <div className="form">
               <form className="register-form">
-                <input type="text" placeholder="name"/>
+                <input type="text" placeholder="Username"/>
                 <input type="password" placeholder="password"/>
-                <input type="text" placeholder="email address"/>
+                <input type="text" placeholder="Firstname"/>
+                <input type="text" placeholder="Secondname"/>
+                <input type="email" placeholder="Email"/>
+                <input type="text" placeholder="Phone"/>
+                <label for="avatar">Choose a profile picture:</label>
+                <input type="file" accept="image/*" id="avatar" />
                 <button>create</button>
                 <p className="message">Already registered? <a href="#">Sign In</a></p>
               </form>
