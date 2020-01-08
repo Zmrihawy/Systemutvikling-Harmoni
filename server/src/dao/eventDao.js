f66f2a646e4708e117a67171631c9c9b0b1936ba
Torje Dahll-Larssøn Thorkildsen <tdthorki@stud.ntnu.no>
2020-01-08 09:24:01 +0100

    Split the dao into a user/event dao

--- server/src/dao/eventDao.js
+++ server/src/dao/eventDao.js
@@ -4,20 +4,16 @@ import Dao from './dao.js';
 module.exports = class ServerDao extends Dao {
 
     /**GET*/
+    getPerformance(sql,callback){
+        super.query(`SELECT * FROM ${PERFORMANCE_TABLE} WHERE ${PERFORMANCE_ARTIST_ID} = ?`, [sql], callback);
+    }
+
     getContract(sql, callback) {
         super.query(`SELECT ${PERFORMANCE_CONTRACT} FROM ${PERFORMANCE_TABLE} WHERE ${PERFORMANCE_EVENT_ID} = ? AND ${PERFORMANCE_ARTIST_ID} = ?`, [sql.event, sql.artist], callback);
     }
 
     getEventContracts(sql, callback) {
         super.query(`SELECT * FROM ${PERFORMANCE_TABLE} WHERE ${EVENT_ID} = ?`, [sql], callback);
-    }
-
-    getUser(sql, callback) {
-        super.query(`SELECT * FROM ${USER_TABLE} WHERE ${USER_EMAIL} = ?`, [sql], callback);
-    }
-
-    getUsers(sql, callback) {
-        super.query(`SELECT * FROM ${USER_TABLE}`, [], callback);
     }
 
     getTickets(sql, callback) {
@@ -41,11 +37,6 @@ module.exports = class ServerDao extends Dao {
     }
 
     /**CREATE*/
-    createUser(sql, callback) {
-        super.query(`INSERT INTO ${USER_TABLE} (${USER_USERNAME},${USER_PASSWORD},${USER_SALT},${USER_EMAIL},${USER_PHONE},${USER_FIRST_NAME},${USER_LAST_NAME}) 
-                    VALUES (?,?,?,?,?,?)`, [sql.username, sql.password, sql.salt, sql.email, sql.phone, sql.firstName, sql.lastName], callback);
-    }
-
     createEvent(sql, callback) {
         super.query(`INSERT INTO ${EVENT_TABLE} (${EVENT_NAME},${EVENT_HOST_ID},${EVENT_START_TIME},${EVENT_END_TIME},${EVENT_LOCATION}) 
                     VALUES (?,?,?) `, [sql.name, sql.userId, sql.startTime, sql.endTime, sql.location], callback);
@@ -67,71 +58,41 @@ module.exports = class ServerDao extends Dao {
     }
 
     /**DELETE*/
-    deleteUser(sql , callback){
-        super.query(`DELETE FROM ${USER_TABLE} WHERE ${USER_EMAIL} = ?`, [sql], callback);
-    }
-
-    deleteRider(sql, callback){
+    deleteRider(sql, callback) {
         super.query(`DELETE FROM ${RIDER_TABLE} WHERE ${RIDER_PERFORMANCE_ID} = ? AND ${RIDER_NAME}`, [sql], callback);
     }
 
-    deleteEvent(sql, callback){
+    deleteEvent(sql, callback) {
         super.query(`DELETE FROM ${EVENT_TABLE} WHERE ${EVENT_ID}`, [sql], callback);
     }
 
-    deleteTicket(sql, callback){
+    deleteTicket(sql, callback) {
         super.query(`DELETE FROM ${TICKET_TABLE} WHERE ${TICKET_NAME} = ? AND ${TICKET_EVENT_ID} = ?`, [sql.name, sql.eventId], callback);
     }
 
-    deletePerformance(sql, callback){
+    deletePerformance(sql, callback) {
         super.query(`DELETE FROM ${PERFORMANCE_TABLE} WHERE ${PERFORMANCE_ARTIST_ID} = ?`, [sql], callback);
     }
 
     /**UPDATE*/
-    updateUsername(sql, callback) {
-        super.query(`UPDATE ${USER_TABLE} SET ${USER_USERNAME} = ? WHERE ${USER_ID} = ?`, [sql.username, sql.userId], callback);
-    }
-
-    updatePassword(sql, callback){
-        super.query(`UPDATE ${USER_TABLE} SET ${USER_PASSWORD} = ? WHERE ${USER_ID} = ?`, [sql.password, sql.userId], callback);
-    }
-
-    updateName(sql, callback){
-        super.query(`UPDATE ${USER_TABLE} SET ${USER_FIRST_NAME} = ?, ${USER_LAST_NAME} = ? WHERE ${USER_ID} = ?`, [sql.firstName, sql.lastName, sql.userId], callback);
-    }
-
-    updateEmail(sql, callback){
-        super.query(`UPDATE ${USER_TABLE} SET ${USER_EMAIL} = ? WHERE ${USER_ID}  = ?`, [sql.email, sql.userId], callback);
-    }
-
-    updatePhone(sql, callback){
-        super.query(`UPDATE ${USER_TABLE} SET ${USER_PHONE} = ? WHERE ${USER_ID} = ?`, [sql.phone, sql.userId], callback);
-    }
-
-    updateUser(sql, callback){
-        let userAtributes = [sql.username, sql.salt,sql.email,sql.phone,sql.firstName,sql.lastName, sql.userId];
-        super.query(`UPDATE ${USER_TABLE} SET ${USER_USERNAME} = ?, ${USER_EMAIL} = ?, ${USER_PHONE} = ?, ${USER_FIRST_NAME}, ${USER_LAST_NAME} = ? WHERE ${USER_ID} = ?`,
-        userAtributes, callback);
-    }
-
-    updateTicket(sql, callback){
+    updateTicket(sql, callback) {
         super.query(`UPDATE ${TICKET_TABLE} SET ${TICKET_PRICE} = ?, ${TICKET_AMOUNT} = ?, ${TICKET_DESCRIPTION} = ?  WHERE ${TICKET_NAME} = ? AND ${TICKET_EVENT_ID} = ? `
-        [sql.ticketPrice, sql.ticketDescription, sql.ticketName, sql.ticketEventId], callback);
+            [sql.ticketPrice, sql.ticketDescription, sql.ticketName, sql.ticketEventId], callback);
     }
 
-    updateRider(sql, callback){
-        super.query(`UPDATE ${RIDER_TABLE} SET ${RIDER_NAME} = ?, ${RIDER_AMOUNT} = ? WHERE ${RIDER_PERFORMANCE_ID} and ${RIDER_NAME} = ?`, 
-        [sql.name, sql.ammount, sql.performanceId, sql.oldName], callback);
+    updateRider(sql, callback) {
+        super.query(`UPDATE ${RIDER_TABLE} SET ${RIDER_NAME} = ?, ${RIDER_AMOUNT} = ? WHERE ${RIDER_PERFORMANCE_ID} and ${RIDER_NAME} = ?`,
+            [sql.name, sql.ammount, sql.performanceId, sql.oldName], callback);
     }
 
-    updatePerformance(sql, callback){
+    updatePerformance(sql, callback) {
         super.query(`UPDATE ${PERFORMANCE_TABLE} SET ${PERFORMANCE_START_TIME} = ?, ${PERFORMANCE_END_TIME} = ?, ${PERFORMANCE_CONTRACT} = ? WHERE ${PERFORMANCE_ID} = ?`,
-        [sql.startTime, sql.endTime, sql.contract, sql.performanceId], callback);
+            [sql.startTime, sql.endTime, sql.contract, sql.performanceId], callback);
     }
 
-    updateEvent(sql, callback){
+    updateEvent(sql, callback) {
         super.query(`UPDATE ${EVENT_TABLE} SET ${EVENT_NAME} = ?, ${EVENT_HOST_ID} = ?, ${EVENT_ACTIVE} = ?, ${EVENT_LOCATION} = ?, ${EVENT_START_TIME} = ?, ${EVENT_END_TIME} = ? WHERE ${EVENT_ID} = ?`,
-        [sql.eventHostId, sql.eventActive, sql.eventLocation, sql.eventStartTime, sql.endTime, sql.eventId], callback);
+            [sql.eventHostId, sql.eventActive, sql.eventLocation, sql.eventStartTime, sql.endTime, sql.eventId], callback);
     }
 
 };
---
+++ server/src/dao/userDao.js
@@ -0,0 +1,48 @@
+import Dao from './dao.js';
+
+module.exports = class UserDao extends Dao {
+
+    getUser(sql, callback) {
+        super.query(`SELECT * FROM ${USER_TABLE} WHERE ${USER_EMAIL} = ?`, [sql], callback);
+    }
+
+    getUsers(sql, callback) {
+        super.query(`SELECT * FROM ${USER_TABLE}`, [], callback);
+    }
+
+    createUser(sql, callback) {
+        super.query(`INSERT INTO ${USER_TABLE} (${USER_USERNAME},${USER_PASSWORD},${USER_SALT},${USER_EMAIL},${USER_PHONE},${USER_FIRST_NAME},${USER_LAST_NAME}) 
+                    VALUES (?,?,?,?,?,?)`, [sql.username, sql.password, sql.salt, sql.email, sql.phone, sql.firstName, sql.lastName], callback);
+    }
+
+    deleteUser(sql, callback) {
+        super.query(`DELETE FROM ${USER_TABLE} WHERE ${USER_EMAIL} = ?`, [sql], callback);
+    }
+
+    updateUsername(sql, callback) {
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_USERNAME} = ? WHERE ${USER_ID} = ?`, [sql.username, sql.userId], callback);
+    }
+
+    updatePassword(sql, callback) {
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_PASSWORD} = ? WHERE ${USER_ID} = ?`, [sql.password, sql.userId], callback);
+    }
+
+    updateName(sql, callback) {
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_FIRST_NAME} = ?, ${USER_LAST_NAME} = ? WHERE ${USER_ID} = ?`, [sql.firstName, sql.lastName, sql.userId], callback);
+    }
+
+    updateEmail(sql, callback) {
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_EMAIL} = ? WHERE ${USER_ID}  = ?`, [sql.email, sql.userId], callback);
+    }
+
+    updatePhone(sql, callback) {
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_PHONE} = ? WHERE ${USER_ID} = ?`, [sql.phone, sql.userId], callback);
+    }
+
+    updateUser(sql, callback) {
+        let userAtributes = [sql.username, sql.salt, sql.email, sql.phone, sql.firstName, sql.lastName, sql.userId];
+        super.query(`UPDATE ${USER_TABLE} SET ${USER_USERNAME} = ?, ${USER_EMAIL} = ?, ${USER_PHONE} = ?, ${USER_FIRST_NAME}, ${USER_LAST_NAME} = ? WHERE ${USER_ID} = ?`,
+            userAtributes, callback);
+    }
+
+};
