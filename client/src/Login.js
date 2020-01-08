0e948031693e547a1a49c9ba88b2b6cc8fd89844
Zmrihawy <zmrihawy@stud.ntnu.no>
2020-01-08 13:37:34 +0100

    #login and registration page

---
+++ client/src/Login.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import './Login.css'; 
+export default class Login extends Component {
+
+
+    render() {
+        
+        return (
+            <div className="login-page">
+            <div className="form">
+           
+              <form className="login-form">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>login</button>
+                <p className="message">Not registered? <a onClick={this.handleClick} href="#">Create an account</a></p>
+              </form>
+            </div>
+          </div>
+        );
+      }
+}
