c2239624e384808965f55d48e84da35effd4447b
Håkon Harnes <hakon@dhcp-10-22-12-254.wlan.ntnu.no>
2020-01-09 14:19:13 +0100

    fixed responsiveness

--- client/src/components/EventInfo/EventInfo.js
+++ client/src/components/EventInfo/EventInfo.js
@@ -4,13 +4,22 @@ import classes from './EventInfo.module.scss';
 import Map from '../Map/Map';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faFile, faLocationArrow, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+    faUser,
+    faFile,
+    faLocationArrow,
+    faCalendarAlt
+} from '@fortawesome/free-solid-svg-icons';
 
 const eventInfo = props => {
     return (
         <div className={classes.container}>
-            <h1 className={classes.title}>{props.title}</h1>
-            <button className={classes.button__edit}>Rediger arrangement</button>
+            <div className={classes.top__section}>
+                <h1 className={classes.title}>{props.title}</h1>
+                <button className={classes.button__edit}>
+                    Rediger arrangement
+                </button>
+            </div>
             <div className={classes.desc}>
                 <h1 className={classes.descTitle}>Om arrangementet</h1>
                 <p>{props.desc}</p>
@@ -19,28 +28,50 @@ const eventInfo = props => {
             <div className={classes.info}>
                 <div className={`${classes.location} ${classes.info__item}`}>
                     <h2 className={classes.info__title}>Lokasjon</h2>
-                    <p className={classes.info__description}>{props.location}</p>
-                    <FontAwesomeIcon className={classes.info__icon} icon={faCalendarAlt} />
+                    <p className={classes.info__description}>
+                        {props.location}
+                    </p>
+                    <FontAwesomeIcon
+                        className={classes.info__icon}
+                        icon={faCalendarAlt}
+                    />
                 </div>
                 <div className={`${classes.date} ${classes.info__item}`}>
                     <h2 className={classes.info__title}>Dato</h2>
                     <p className={classes.info__description}>{props.date}</p>
-                    <FontAwesomeIcon className={classes.info__icon} icon={faLocationArrow} />
+                    <FontAwesomeIcon
+                        className={classes.info__icon}
+                        icon={faLocationArrow}
+                    />
                 </div>
                 <div className={`${classes.host} ${classes.info__item}`}>
                     <h2 className={classes.info__title}>Arrangør</h2>
                     <p className={classes.info__description}>{props.host}</p>
-                    <FontAwesomeIcon className={classes.info__icon} icon={faUser} />
+                    <FontAwesomeIcon
+                        className={classes.info__icon}
+                        icon={faUser}
+                    />
                 </div>
-                <div className={`${classes.ticketAmount} ${classes.info__item}`}>
+                <div
+                    className={`${classes.ticketAmount} ${classes.info__item}`}
+                >
                     <h2 className={classes.info__title}>Antall billetter</h2>
-                    <p className={classes.info__description}>{props.ticketAmount}</p>
-                    <FontAwesomeIcon className={classes.info__icon} icon={faFile} />
+                    <p className={classes.info__description}>
+                        {props.ticketAmount}
+                    </p>
+                    <FontAwesomeIcon
+                        className={classes.info__icon}
+                        icon={faFile}
+                    />
                 </div>
             </div>
 
             <div className={classes.map}>
-                <Map longitude={props.longitude} latitude={props.latitude} location={props.location} />
+                <Map
+                    longitude={props.longitude}
+                    latitude={props.latitude}
+                    location={props.location}
+                />
             </div>
 
             <div className={classes.artists}>
@@ -53,8 +84,12 @@ const eventInfo = props => {
                                 <img src={artist.image} alt={artist.name}></img>
                             </div>
                             <h3>{artist.name}</h3>
-                            <button className={classes.button__contract}>Kontrakt</button>
-                            <button className={classes.button__rider}>Rider</button>
+                            <button className={classes.button__contract}>
+                                Kontrakt
+                            </button>
+                            <button className={classes.button__rider}>
+                                Rider
+                            </button>
                         </div>
                     ))}
                 </div>
@@ -74,14 +109,18 @@ const eventInfo = props => {
                 </div>
             </div>
 
-               <div className={classes.staff}>
+            <div className={classes.staff}>
                 <h1 className={classes.subtitle}>Personell</h1>
 
                 <div className={classes.staff__list}>
                     {props.staff.map((staff, i) => (
                         <div className={classes.staff__wrapper} key={i}>
-                            <h2 className={classes.staff__name}>{staff.name}</h2>
-                            <p className={classes.staff_proffesion}>{staff.proffesion}</p>
+                            <h2 className={classes.staff__name}>
+                                {staff.name}
+                            </h2>
+                            <p className={classes.staff_proffesion}>
+                                {staff.proffesion}
+                            </p>
                         </div>
                     ))}
                 </div>
