ef1e7aaeff44254270e99a0e27c9a16b2c1b0eef
SveinJH <43957538+SveinJH@users.noreply.github.com>
2020-01-14 10:04:22 +0100

    Added modal and backdrop components

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -11,28 +11,73 @@ export default class NavBar extends Component {
     }
     render() {
         return (
-            <div id="container">
+            <>
                 <div className={classes.navBar}>
-                    <div className={classes.activeLink} id='eventLink' onClick={event => {this.changePage('eventLink')}}>Mine Arrangement</div>
-                    <div className={classes.navLink} id='createLink' onClick={event => {this.changePage('createLink')}}>Lage Arrangement</div>
-                    <div className={classes.navLink} id='profileLink' onClick={event => {this.changePage('profileLink')}}>Min Profil</div>
-                    <div className={classes.navLink} id='logLink' onClick={event => {this.changePage('logLink')}}>Logge Ut</div>
+                    <div
+                        className={classes.activeLink}
+                        id="eventLink"
+                        onClick={event => {
+                            this.changePage('eventLink');
+                        }}
+                    >
+                        Mine Arrangement
+                    </div>
+                    <div
+                        className={classes.navLink}
+                        id="createLink"
+                        onClick={event => {
+                            this.changePage('createLink');
+                        }}
+                    >
+                        Lage Arrangement
+                    </div>
+                    <div
+                        className={classes.navLink}
+                        id="profileLink"
+                        onClick={event => {
+                            this.changePage('profileLink');
+                        }}
+                    >
+                        Min Profil
+                    </div>
+                    <div
+                        className={classes.navLink}
+                        id="logLink"
+                        onClick={event => {
+                            this.changePage('logLink');
+                        }}
+                    >
+                        Logge Ut
+                    </div>
                 </div>
-                <br/>
-            </div>
-        )
+            </>
+        );
     }
-    changePage(id){
-        if(document.getElementsByClassName("" + classes.activeLink + "") !== null){
-            if(document.getElementById(id) !== null){
-                document.getElementsByClassName(classes.activeLink)[0].className = "" + classes.navLink + "";
-                document.getElementById(id).className = "" + classes.activeLink + "";
+    changePage(id) {
+        if (
+            document.getElementsByClassName('' + classes.activeLink + '') !==
+            null
+        ) {
+            if (document.getElementById(id) !== null) {
+                document.getElementsByClassName(
+                    classes.activeLink
+                )[0].className = '' + classes.navLink + '';
+                document.getElementById(id).className =
+                    '' + classes.activeLink + '';
 
-                switch(id){
-                    case 'profileLink': history.push('/user/id'); break;
-                    case 'createLink': history.push('/registrerarrangement'); break;
-                    case 'eventLink': history.push('/arrangement/id'); break;
-                    case 'logLink': history.push('/'); break;
+                switch (id) {
+                    case 'profileLink':
+                        history.push('/user/id');
+                        break;
+                    case 'createLink':
+                        history.push('/registrerarrangement');
+                        break;
+                    case 'eventLink':
+                        history.push('/arrangement/id');
+                        break;
+                    case 'logLink':
+                        history.push('/');
+                        break;
                 }
             }
         }
