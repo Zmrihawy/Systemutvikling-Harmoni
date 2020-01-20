91fdfac32f8d5995466bc0c33d3dbadc9e64ff69
Håkon Harnes <hakhar@hotmail.com>
2020-01-20 14:31:04 +0100

    added staff edit

--- client/src/components/StaffEdit/StaffEdit.js
+++ client/src/components/StaffEdit/StaffEdit.js
@@ -3,54 +3,67 @@ import React from 'react';
 import classes from '../StaffEdit/StaffEdit.module.scss';
 
 const staffEdit = props => {
-    console.log(props);
+    console.log(props); 
 
     return (
         <div className={classes.container}>
             <h1 className={classes.title}>Rediger personell</h1>
+
             <div className={classes.desc__wrapper}>
                 <p className={classes.ph__name}>Navn</p>
                 <p className={classes.ph__contact}>Kontakt info</p>
                 <p className={classes.ph__profession}>Yrke</p>
             </div>
-            <div className={classes.staff__list}>
-                {props.staff.map(e => (
-                    <form className={classes.staff__wrapper}>
-                        <input
-                            className={classes.input__name}
-                            type="text"
-                            name="name"
-                            value={e.name}
-                            onChange={props.handleChange}
-                        />
-                        <input
-                            className={classes.input__contactInfo}
-                            type="text"
-                            name="contact info"
-                            value={e.contactInfo}
-                            onChange={props.handleChange}
-                        />
+            <form className={classes.form} onSubmit={props.handleButtonSubmitClick}>
+                <div className={classes.staff__list}>
+                    {props.staff.map((e, i) => (
+                        <div className={classes.staff__wrapper} key={i} id={i}>
+                            <input
+                                className={classes.input__name}
+                                type="text"
+                                name="name"
+                                value={e.name}
+                                onChange={props.handleChange}
+                                required
+                            />
+                            <input
+                                className={classes.input__contactInfo}
+                                type="text"
+                                name="contactInfo"
+                                value={e.contactInfo}
+                                onChange={props.handleChange}
+                                required
+                            />
 
-                        <input
-                            className={classes.input__profession}
-                            type="text"
-                            name="proffesion"
-                            value={e.profession}
-                            onChange={props.handleChange}
-                        />
-                        <input
-                            className={classes.button__delete}
-                            type="submit"
-                            value="slett"
-                        />
-                    </form>
-                ))}
-            </div>
-            <input
-                className={classes.button__submit}
-                type="submit"
-                value="Bekreft"
-            />
+                            <input
+                                className={classes.input__profession}
+                                type="text"
+                                name="proffesion"
+                                value={e.profession}
+                                onChange={props.handleChange}
+                                required
+                            />
+                            <input
+                                className={classes.button__delete}
+                                onClick={props.handleButtonDeleteClick}
+                                type="button"
+                                value="slett"
+                            />
+                        </div>
+                    ))}
+                </div>
+                <input
+                    className={classes.button__add}
+                    onClick={props.handleButtonAddClick}
+                    type="button"
+                    value="+"
+                />
+                <input
+                    className={classes.button__submit}
+                    type="submit"
+                    value="Bekreft endringer"
+                />
+            </form>
         </div>
     );
 };
