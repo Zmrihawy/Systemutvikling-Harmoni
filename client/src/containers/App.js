0c236996bbce6338268c7df5d2f941ca59887892
Zmrihawy <zmrihawy@stud.ntnu.no>
2020-01-20 15:35:12 +0100

    Add erorrComponent

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -4,6 +4,7 @@ import Main from './Main/Main';
 import classes from './App.module.scss';
 import ViewProfile from './ViewProfile/ViewProfile';
 import NavBar from '../components/NavBar/NavBar';
+import ErrorBoundary from '../components/ErorrBoundary/ErorrBoundary';
 
 import { createHashHistory } from 'history';
 export const history = createHashHistory();
@@ -11,7 +12,9 @@ export const history = createHashHistory();
 function App() {
     return (
         <div>
+            <ErrorBoundary>
             <Main />
+            </ErrorBoundary>
         </div>
     );
 }
