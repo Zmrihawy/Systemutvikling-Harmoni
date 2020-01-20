3e42bbabd00c7f6b3af6739a2c4e8469cb3092f5
Svein Jakob Høie <sveinjakobhoie@gmail.com>
2020-01-20 13:23:19 +0100

    Added spinner component

---
+++ client/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Spinner.module.scss';
+
+const spinner = () => {
+    return <div class={classes.Loader}>Loading...</div>;
+};
+
+export default spinner;
